6d0b804b16ea05d77d388fe3460b33cac2974365
Tim <dalytimdaly@gmail.com>
2022-09-16 16:16:20 -0600

    commit tree d403921dfb972c88b67a025976d52a19dcc0bfed

--- methods.rb
+++ methods.rb
@@ -1,1 +1,23 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(x, y)
+    return x + y
+end
+
+def halve(x)
+    return nil unless x.class == Integer
+
+    
+    x / 2
+end
